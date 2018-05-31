a7df14ea7c59ab9b05c8bb491016716076d80a31
haibzhou <haibzhou@ebay.com>
2018-05-31 17:18:50 +0800

    init: ass fsp binary
    fsp is a fake secure proxy, it is used to act as fake https server
    by contacting a insecure backend.

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var cert string
+var key string
+
+var proxy string
+
+func init() {
+	flag.StringVar(&cert, "cert", "", "the path to certificate")
+	flag.StringVar(&key, "key", "", "the path to key file")
+	flag.StringVar(&proxy, "proxy", "", "the ip address of proxy server")
+}
+
+func main() {
+	flag.Parse()
+
+	target, err := url.Parse("http://" + proxy)
+	if err != nil {
+		log.Fatalf("failed to parse proxy %q: %s", proxy, err)
+	}
+
+	upstream := httputil.NewSingleHostReverseProxy(target)
+	http.ListenAndServeTLS(":443", cert, key, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		upstream.ServeHTTP(w, r)
+	}))
+}
